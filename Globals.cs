63973dc05bffda44ae67cd1e79c1e4744f94771b
Miguel Cunha <josemiguelpereirasilvacunha@gmail.com>
2024-05-06 20:31:19 +0100

    MapLoader class. Create tile maps from txt files.

---
+++ Globals.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSC
+{
+    internal class Globals
+    {
+        // Tile map dimensions
+        public static int MapWidth = 0;
+        public static int MapHeight = 0;
+
+        // Tile size in pixels
+        public const int TileWidth = 16;
+        public const int TileHeight = 16;
+
+        public static float _scaleSprites = 2.5f; // Scale factor
+        public static float tile_scale_factor = 2.0f;
+    }
+}
